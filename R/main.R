f4d87d6bba7576c5e5e168aac7a20452341a33fa
Tyson Barrett <t.barrett88@gmail.com>
2024-10-27 17:07:18 -0600

    add separate channels

--- R/main.R
+++ R/main.R
@@ -1,3 +1,60 @@
+#' @title Split Channels
+#'
+#' @description Splits the wav file into the two channels
+#'
+#' @param wav_file The path to the wav file
+#' @param noise_reduction whether the praat noise reduction script should be run before getting boundaries, default = FALSE
+#' @param threshold noise level for removal
+#' @param plot should the tuneR::plot() be made for each channel? Default is FALSE.
+#'
+#' @import tuneR
+#' @importFrom stringr str_replace
+#'
+#' @export
+split_channels <- function(wav_file, noise_reduction = FALSE, threshold = 200, plot = FALSE){
+  # read in wave file
+  wav <- tuneR::readWave(wav_file)
+  wav <- tuneR::normalize(wav, unit = "16")
+  # check if it has two channels
+  if (tuneR::nchannel(wav) != 2) stop ("wave file needs to have 2 channels")
+
+  # file names
+  ch1 <- stringr::str_replace(wav_file, "\\.wav$", "_ch1.wav")
+  ch2 <- stringr::str_replace(wav_file, "\\.wav$", "_ch2.wav")
+
+  # split into left and right
+  left <- tuneR::channel(wav, which = "left")
+  right <- tuneR::channel(wav, which = "right")
+
+  # save channeled wav files
+  tuneR::writeWave(left, ch1)
+  tuneR::writeWave(right, ch2)
+
+  if (plot) tuneR::plot(left); if (plot) tuneR::plot(right)
+
+  # delete tmp folder if already exists
+  if (fs::dir_exists(file.path(fs::path_dir(wav_file), "tmp")))
+    fs::dir_delete(file.path(fs::path_dir(wav_file), "tmp"))
+
+  # move files to tmp folder
+  fs::dir_create(file.path(fs::path_dir(wav_file), "tmp"))
+  file1 = fs::path_file(ch1)
+  file2 = fs::path_file(ch2)
+  fs::file_move(ch1, file.path(fs::path_dir(ch1), "tmp"))
+  fs::file_move(ch2, file.path(fs::path_dir(ch2), "tmp"))
+
+  # new locations
+  ch1 = file.path(fs::path_dir(ch1), "tmp", file1)
+  ch2 = file.path(fs::path_dir(ch2), "tmp", file2)
+
+  # check that files exist
+  if (! fs::file_exists(ch1) | ! fs::file_exists(ch2)) stop("error creating channels")
+
+  # return names
+  return(c(ch1, ch2))
+}
+
+
 # Define package structure
 #' @title praatsyllable: R Package for Syllable Detection using Praat
 #' @description Integrates the Praat Syllable Nuclei script for detecting syllables in audio files
@@ -60,13 +117,19 @@ detect_syllables <- function(wav_file,
   wav_dir <- dirname(wav_file)
   wav_name <- basename(wav_file)
 
+  # split channels
+  chs <- split_channels(wav_file)
+  fs::dir_create(file.path(wav_dir, "tmp"))
+  fs::file_move(chs[[1]], file.path(wav_dir, "tmp"))
+  fs::file_move(chs[[2]], file.path(wav_dir, "tmp"))
+
   # Build Praat command
   praat_cmd <- sprintf('"%s" --run "%s" %f %f 1 "%s"',
                        praat_path,
                        script_path,
                        ignorance_level,
                        min_dip,
-                       wav_dir)
+                       file.path(wav_dir, "tmp"))
 
   # Run Praat
   system_result <- system(praat_cmd)
